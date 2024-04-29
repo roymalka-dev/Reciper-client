20718b46c936f176a97922aab45f70f46931c929
Roy Malka <roy.malka@roy.malka-MBP>
2024-04-29 16:17:08 +0300

    env fix

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,13 +1,14 @@
-const NODE_ENV = import.meta.env.VITE_NODE_ENV || "DEV";
+//const NODE_ENV = import.meta.env.VITE_NODE_ENV || "DEV";
 
-const DEV_API = import.meta.env.VITE_APP_API_URL;
+//const DEV_API = import.meta.env.VITE_APP_API_URL;
 //const PROD_API = import.meta.env.VITE_APP_API_URL_PROD;
-const PROD_API = process.env.VITE_APP_API_URL_PROD;
+//const PROD_API = process.env.VITE_APP_API_URL_PROD;
 
-const API = NODE_ENV === "DEV" ? DEV_API : PROD_API;
+//const API = NODE_ENV === "DEV" ? DEV_API : PROD_API;
 
 export const appConfig = {
-  apiBaseUrl: API,
+  apiBaseUrl: "https://reciper-api.roymalka.dev",
+  //apiBaseUrl: "http://localhost:3000",
   apiPrefix: "/api",
   unAuthenticatedEntryPath: "/auth/sign-in",
 };
