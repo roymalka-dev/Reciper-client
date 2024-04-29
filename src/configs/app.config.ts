19c2586990d04b22120a7cc9b0e22d49f8d2640c
Roy Malka <roy.malka@roy.malka-MBP>
2024-04-29 16:03:20 +0300

    api url

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,5 +1,12 @@
+const NODE_ENV = import.meta.env.VITE_NODE_ENV || "DEV";
+
+const DEV_API = import.meta.env.VITE_APP_API_URL;
+const PROD_API = import.meta.env.VITE_APP_API_URL_PROD;
+
+const API = NODE_ENV === "DEV" ? DEV_API : PROD_API;
+
 export const appConfig = {
-  apiBaseUrl: "http://localhost:3000",
+  apiBaseUrl: API,
   apiPrefix: "/api",
   unAuthenticatedEntryPath: "/auth/sign-in",
 };
