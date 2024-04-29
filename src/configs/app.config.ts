6ba68107540892eda89e546c6e587493f2b7100e
Roy Malka <roy.malka@roy.malka-MBP>
2024-04-29 16:11:20 +0300

    base url config

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,7 +1,8 @@
 const NODE_ENV = import.meta.env.VITE_NODE_ENV || "DEV";
 
 const DEV_API = import.meta.env.VITE_APP_API_URL;
-const PROD_API = import.meta.env.VITE_APP_API_URL_PROD;
+//const PROD_API = import.meta.env.VITE_APP_API_URL_PROD;
+const PROD_API = process.env.VITE_APP_API_URL_PROD;
 
 const API = NODE_ENV === "DEV" ? DEV_API : PROD_API;
 
